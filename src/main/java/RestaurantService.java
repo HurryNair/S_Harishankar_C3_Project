c374cb0c9ac20a99e26e43c827518af9578936b9
Harishankar S <harishankar.s@hpe.com>
2022-02-02 18:37:25 +0530

    Part 3: Solution

--- src/main/java/RestaurantService.java
+++ src/main/java/RestaurantService.java
@@ -31,4 +31,28 @@ public class RestaurantService {
         return restaurants;
     }
 
+    public int findOrderValue(Restaurant restaurant, String... pickedItems) {
+        int orderValue = 0;
+        for (String item : pickedItems) {
+            Item pickedItem = findItemByName(restaurant, item);
+            orderValue += pickedItem.getPrice();
+        }
+        return orderValue;
+    }
+
+    // Utility function to fetch a specific item obj by its name from the associated
+    // restaurant obj
+    private Item findItemByName(Restaurant restaurant, String item) {
+        List<Item> menuItems = new ArrayList<Item>();
+        Item foundItem = null;
+        menuItems = restaurant.getMenu();
+        for (Item menuItem : menuItems) {
+            if (menuItem.getName().equals(item)) {
+                foundItem = menuItem;
+                break;
+            }
+        }
+        return foundItem;
+    }
+
 }
